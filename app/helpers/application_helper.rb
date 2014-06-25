0aca4d4d0015f8a429f5922034ebc0653322dfbe
Guillermo Esteves <guillermo.esteves@sbnation.com>
2014-06-25 18:16:25 -0400

    Add some helpers to render font variations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def font_variation_to_string(v)
+    style = font_style_to_string(v[0])
+    weight = v[1].to_i * 100
+    "#{style} #{weight}"
+  end
+
+  def font_variation_to_css(v)
+    style = font_style_to_string(v[0])
+    weight = v[1].to_i * 100
+    "font-style:#{style};font-weight:#{weight};"
+  end
+
+  def font_style_to_string(style)
+    case style
+    when "n"
+      "normal"
+    when "i"
+      "italic"
+    when "o"
+      "oblique"
+    end
+  end
 end
